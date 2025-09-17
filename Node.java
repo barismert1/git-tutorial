9a44e606c6a8025190f610bad10fe13c1124da6e
Barış Mert <baris.mert@std.yeditepe.edu.tr>
2025-09-17 18:35:39 +0300

    added Linkedlist+node

---
+++ Node.java
@@ -0,0 +1,26 @@
+package cis112_2025_1_e7final_List_Q2_g2;
+
+/**
+ * A node having data as String.
+ * 
+ * @author bingol
+ */
+public class Node {
+
+	public int data;
+	public Node next;
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+
+	@Override
+	public String toString() {
+		return "[Node: data=" + data + "]";
+	}
+
+	public int canonical() {
+		return data;
+	}
+}
